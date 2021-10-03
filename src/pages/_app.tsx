8829deec01c0211ca0dcd6cb66dc52f3d66b82a2
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2021-10-02 21:10:41 -0300

    aula-4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Provider as NextAutoProvider } from "next-auth/client";
 
 import "../../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <NextAutoProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAutoProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
-import { SubscribeButton } from "../components/SubscribeButton";
-import styles from './home.module.scss'
 
-export default function Home() {
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -12,16 +23,38 @@ export default function Home() {
         <section className={styles.hero}>
           <span> 👏 Hey, welcome</span>
           <h1>
-            News about <br/>the <span>React</span> world.
+            News about <br />
+            the <span>React</span> world.
           </h1>
           <p>
-            Get access to all the publications <br/>
-            <span>for $9.90 mouth</span>
+            Get access to all the publications <br />
+            <span>for {product.amount} mouth</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/imagens/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve(
+    "price_1JfCcsJFNyEr4efTgl1xeVG4",
+    {}
+  );
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24horas
+  };
+};
